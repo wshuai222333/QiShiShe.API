8301b20c43e00c103e7887de1859f665f3f5e972
wshuai <wangshuai@reapal.com>
2019-01-29 15:44:24 +0800

    123123

---
+++ Travel.Api.DTO/Enterprise/Request/Order/RequestGetDemandOrderList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QiShiShe.Api.DTO.Enterprise.Request.Order {
+    public class GetDemandOrderList : RequestOriBaseModel {
+        public int pageindex { get; set; }
+
+        public int pagesize { get; set; }
+
+        public int EnterpriseId { get; set; }
+    }
+}
---
+++ Travel.Api.Service/Enterprise/Order/GetDemandOrderListService.cs
@@ -0,0 +1,18 @@
+﻿using QiShiShe.Api.DTO.Enterprise.Request.Order;
+using QiShiShe.PetaPoco.Repositories.QiShiShe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QiShiShe.Api.Service.Enterprise.Order {
+    public class GetDemandOrderListService : ApiOriBase<RequestGetDemandOrderList> {
+        #region 注入服务
+        public DemandOrderRep demandOrderRep { get; set; }
+        #endregion
+        /// <summary>
+        /// 执行方法
+        protected override void ExecuteMethod() {
+            this.Result.Data = demandOrderRep.GetEnterpriseList(this.Parameter.EnterpriseId, this.Parameter.pageindex, this.Parameter.pagesize);
+        }
+    }
+}
