03dbe5b10bd2917416eb943ecd67dd60ab4d2fb4
unknown <wangshuai@reapal.com>
2019-02-16 15:35:53 +0800

    123

---
+++ Travel.Api.DTO/Boss/Request/Order/RequestUpdateDemandOrderStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QiShiShe.Api.DTO.Boss.Request.Order
+{
+    public class RequestUpdateDemandOrderStatus : RequestOriBaseModel
+    {
+        public string OrderId { get; set; }
+
+        public int Status { get; set; }
+    }
+}
---
+++ Travel.Api.Service/Boss/Order/UpdateDemandOrderStatusService.cs
@@ -0,0 +1,18 @@
+﻿using QiShiShe.Api.DTO.Boss.Request.Order;
+using QiShiShe.PetaPoco.Repositories.QiShiShe;
+
+namespace QiShiShe.Api.Service.Boss
+{
+    public class UpdateDemandOrderStatusService : ApiOriBase<RequestUpdateDemandOrderStatus>
+    {
+        #region 注入服务
+        public DemandOrderRep demandOrderRep { get; set; }
+        #endregion
+        /// <summary>
+        /// 执行方法
+        protected override void ExecuteMethod()
+        {
+            this.Result.Data = demandOrderRep.UpdateDemandOrderStatus(this.Parameter.OrderId,this.Parameter.Status);
+        }
+    }
+}
